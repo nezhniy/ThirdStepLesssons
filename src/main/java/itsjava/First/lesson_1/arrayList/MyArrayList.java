9b3d58185bfc6381f922b4355246584ef97bfbb6
nezhniy <nezhniystrastniy@yandex.ru>
2022-10-05 22:32:06 +0500

    rework 1.1 task

--- src/main/java/itsjava/First/lesson_1/arrayList/MyArrayList.java
+++ src/main/java/itsjava/First/lesson_1/arrayList/MyArrayList.java
@@ -1,9 +1,5 @@
 package itsjava.First.lesson_1.arrayList;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class MyArrayList {
     public static final int DEFAULT_CAPACITY = 10;
     private int realSize;
@@ -20,9 +16,9 @@ public class MyArrayList {
 
     public boolean isEmpty() {
         if (realSize > 0){
-            return true;
-        }else {
             return false;
+        }else {
+            return true;
         }
     }
 
@@ -46,37 +42,97 @@ public class MyArrayList {
     }
 
     public boolean remove(Object o) {
-        return false;
+        int delIndex = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null && array[i].equals(o)){
+                delIndex = i;
+                break;
+            }
+        }
+        for (int i = delIndex; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+
+        if (delIndex == -1){
+            return false;
+        } else {
+            realSize--;
+            return true;
+        }
     }
 
     public void clear() {
         for (int i = 0; i < array.length; i++) {
             array[i] = null;
         }
+        realSize = 0;
     }
 
     public Object get(int index) {
-        return null;
+        Object obj = array[index];
+        return obj;
     }
 
     public Object set(int index, Object element) {
-        return null;
+        array[index] = element;
+        Object obj = array[index];
+        return obj;
     }
 
     public void add(int index, Object element) {
-
+        if (realSize == array.length){
+            Object[] resArray = new Object[array.length + 1];
+            resArray[index] = element;
+            for (int i = 0; i < index; i++) {
+                resArray[i] = array[i];
+            }
+            for (int i = index; i < array.length; i++) {
+                resArray[i + 1] = array[i];
+            }
+            array = resArray;
+        }
     }
 
     public Object remove(int index) {
-        return null;
+        checkIndex(index);
+        Object resElement = array[index];
+        for (int i = index; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        realSize--;
+        return resElement;
+    }
+
+    private void checkIndex(int index) {
+        if (!isCoorectIndex(index)){
+            throw new ArrayIndexOutOfBoundsException("Некорректный индекс");
+        }
+    }
+
+    private boolean isCoorectIndex(int index){
+        if ((index > -1) && (index < realSize)){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public int indexOf(Object o) {
-        return 0;
+            for (int i = 0; i < realSize; i++) {
+                if (array[i].equals(o)) {
+                    return i;
+                }
+            }
+        return -1;
     }
 
     public int lastIndexOf(Object o) {
-        return 0;
+        for (int i = realSize; i >= 0; i--) {
+            if (array[i] == o) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
--- src/main/java/itsjava/First/lesson_1/arrayList/MyArrayListPractice.java
+++ src/main/java/itsjava/First/lesson_1/arrayList/MyArrayListPractice.java
@@ -2,26 +2,73 @@ package itsjava.First.lesson_1.arrayList;
 
 public class MyArrayListPractice {
     public static void main(String[] args) {
-        MyArrayList list = new MyArrayList();
-        System.out.println(list.size());
-        list.add("privet");
-        list.add("poka");
-        System.out.println(list.size());
-        list.add("poka");
-        list.add("poka");
-        list.add("poka");
-        list.add("poka");
-        list.add("poka");
-        list.add("poka");
-        list.add("poka");
-        list.add("poka");
-        list.add("poka");
-        System.out.println(list.size());
+//        MyArrayList list = new MyArrayList();
+//        System.out.println(list.size());
+//        list.add("privet");
+//        list.add("poka");
+//        System.out.println(list.size());
+//        list.add("poka");
+//        list.add("poka");
+//        list.add("poka");
+//        list.add("poka");
+//        list.add("poka");
+//        list.add("poka");
+//        list.add("poka");
+//        list.add("poka");
+//        list.add("poka");
+//        System.out.println(list.size());
+//
+//        System.out.println(list);
+//        System.out.println(list.isEmpty());
+//        System.out.println(list.contains("poka"));
+////        list.clear();
+////        System.out.println(list);
+//        list.remove("privet");
+//        System.out.println(list);
+//
+//        list.add("bye");
+//        System.out.println(list);
+//        list.remove(10);
+//        System.out.println(list);
+//        list.remove(1);
+//        System.out.println(list);
 
+        MyArrayList list = new MyArrayList();
+        list.add("elem1");
+        list.add("elem2");
+        list.add("elem3");
+        list.add("elem4");
+        list.add("poka");
+        list.add("poka");
+        list.add("poka");
+        list.add("poka");
+
+//        list.remove(1);
+//        System.out.println(list);
+//
+//        list.remove(0);
+//        System.out.println(list);
+//        list.remove("elem1");
+//        list.remove("elem3");
+//        System.out.println(list);
+
+        System.out.println(list.indexOf("poka"));
+        System.out.println(list.indexOf("ppp"));
+
+        System.out.println(list.lastIndexOf("poka"));
+
+        System.out.println(list.get(0));
+
+        list.set(0, "asdf");
         System.out.println(list);
+
+        //в чате проигнорировали, в интернете не нашел, почему мой код не работает, нашел как такой же код пишут на с++ и все работает
+        list.add(2, "ooo");
+        System.out.println(list);
+
         System.out.println(list.isEmpty());
-        System.out.println(list.contains("poka"));
+
         list.clear();
-        System.out.println(list);
+        System.out.println(list.size());
     }
 }
