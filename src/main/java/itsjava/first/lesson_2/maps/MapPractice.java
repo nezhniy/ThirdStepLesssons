8dba195b7b765ccc26babaebc349849a7d95b90f
nezhniy <nezhniystrastniy@yandex.ru>
2022-10-12 20:07:26 +0500

    2.5 task

--- src/main/java/itsjava/first/lesson_2/maps/MapPractice.java
+++ src/main/java/itsjava/first/lesson_2/maps/MapPractice.java
@@ -52,6 +52,7 @@ public class MapPractice {
         Client borya = new Client("Borya");
         Client tolya = new Client("Tolya");
         Client kolya = new Client("Kolya");
+        Client alexander = new Client("Alexander");
 
         Map<Client, Fruit> market1 = new HashMap<>();
 
@@ -59,7 +60,8 @@ public class MapPractice {
         market1.put(misha, banana);
         market1.put(borya, peach);
         market1.put(tolya, apricot);
-        market1.put(kolya, kiwi);
+        market1.put(kolya, apple);
+        market1.put(alexander, kiwi);
 
         Map<Client, Fruit> market = new HashMap<>();
 
@@ -67,8 +69,8 @@ public class MapPractice {
 
         System.out.println("market.get(andrew).getName() = " + market.get(andrew).getName());
 
-        market.remove(tolya);
-        System.out.println(market.toString());
+//        market.remove(tolya);
+//        System.out.println(market.toString());
 
         System.out.println(market.containsValue(banana));
         System.out.println(market.containsValue(melon));
@@ -84,5 +86,70 @@ public class MapPractice {
         for (Map.Entry<Client, Fruit> prod: market.entrySet()) {
             System.out.println(prod.getKey() + ": " + prod.getValue());
         }
+
+        //HW 2.5
+        //6
+        int count = 0;
+        for (Client keyClient: market.keySet()) {
+            if (keyClient.getName().length() > 5){
+                count++;
+            }
+        }
+        System.out.println("name length more then 5: " + count);
+
+        count = 0;
+        for (Fruit valueFruit: market.values()) {
+            if (valueFruit.equals(banana)){
+                count++;
+            }
+        }
+        System.out.println("has banana: " + count);
+
+        //7
+        count = 0;
+        for (Map.Entry<Client, Fruit> pair: market.entrySet()) {
+            if (pair.getValue().getName().equals("Apple") && count != 2){
+                count++;
+                continue;
+            }
+            System.out.println(pair.getKey() + ": " + pair.getValue());
+        }
+        System.out.println();
+        count = 0;
+        for (Map.Entry<Client, Fruit> pair: market.entrySet()) {
+            if (pair.getKey().getName().startsWith("A") && count != 2){
+                count++;
+                continue;
+            }
+            System.out.println(pair.getKey() + ": " + pair.getValue());
+        }
+
+        //8
+        System.out.println();
+        for (Map.Entry<Client, Fruit> pair: market.entrySet()) {
+            if (pair.getValue().getName().equalsIgnoreCase("apple")){
+                System.out.println(pair.getKey() + ": " + pair.getValue());
+            }
+        }
+
+        //9
+        System.out.println();
+        Map<Client, Fruit> market2 = new HashMap<>();
+        for (Map.Entry<Client, Fruit> pair: market.entrySet()) {
+            if (pair.getValue().getName().equals("Apple") || pair.getValue().getName().equals("Apricot")){
+                market2.put(pair.getKey(), pair.getValue());
+            }
+        }
+        System.out.println(market2.toString());
+
+        //10
+        count = 0;
+        int sum = 0;
+        for (Map.Entry<Client, Fruit> pair: market.entrySet()) {
+            count++;
+            int weightOfFruit = pair.getValue().getWeight();
+            sum = sum + weightOfFruit;
+        }
+        System.out.println(sum/count);
     }
 }
